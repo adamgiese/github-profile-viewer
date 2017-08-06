16cbfbb1d20b1515e8f253db67ddb8702a386214
Adam Giese <adam.giese@gmail.com>
2017-08-06 13:05:27 -0500

    Add RepoDetails component

--- src/js/components/Repo.js
+++ src/js/components/Repo.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import CommitsContainer from '../containers/CommitsContainer';
+import RepoDetails from './RepoDetails';
 import store from '../store';
 import { fetchCommits } from '../actions';
 /* eslint-ensable no-unused-vars */
@@ -19,8 +20,13 @@ export default class Repo extends React.Component {
   }
 
   render() {
+    const details = this.props.repos ? this.props.repos.find(
+      repo => repo.name === this.props.match.params.repo
+    ) : false;
+
     return (
       <div className='repo'>
+        { details ? <RepoDetails details={details} /> : '' }
         <CommitsContainer />
       </div>
     );
---
+++ src/js/components/RepoDetails.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+/* eslint-ensable no-unused-vars */
+
+const RepoDetails = props => (
+  <div className='repo-details'>
+    <h2 className='repo-details--title'>{props.details.name}</h2>
+  </div>
+);
+
+export default RepoDetails;
