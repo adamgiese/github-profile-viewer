ce9fd287f3055ac90299edf1e7b3133e025f9b23
Adam Giese <adam.giese@gmail.com>
2017-08-05 22:40:49 -0500

    Additional Repo styling

--- src/js/components/RepoList.js
+++ src/js/components/RepoList.js
@@ -8,20 +8,22 @@ const RepoList = (props) => {
     const repos = props.repos.map(
       repo => (
         <li
-          className='repo--card'
+          className='repo-card'
           key={repo.id}
         >
-          <h2><Link to={`${repo.name}/commits`}>{repo.name}</Link></h2>
-          <p>{repo.language}</p>
-          <div className='counts'>
-            <span className='count--watchers'>{repo.watchers_count} watchers</span>
-            <span className='count--forks'>{repo.forks_count} forks</span>
-          </div>
-          { repo.description ? <hr /> : '' }
-          { repo.description ? <p className='repo-description'>{repo.description}</p> : '' }
-          <hr />
-          { repo.homepage ? <a href={repo.homepage}>View Project</a> : '' }
-          <a href={repo.svn_url}>Git Home</a>
+          <h2 className='repo-card--title'><Link to={`${repo.name}/commits`}>{repo.name}</Link></h2>
+          <p className='repo-card--language'>{repo.language}</p>
+          { repo.description ? <hr className='repo-card--rule' /> : '' }
+          { repo.description ? <p className='repo-card--description'>{repo.description}</p> : '' }
+          <hr className='repo-card--rule'/>
+          <ul className='repo-card--counts'>
+            <li className='repo-card--count'>{repo.watchers_count} watchers</li>
+            <li className='repo-card--count'>{repo.forks_count} forks</li>
+          </ul>
+          <ul className='repo-card--links'>
+            { repo.homepage ? <li className='repo-card--link'><a href={repo.homepage}>View Project</a></li> : '' }
+            <li className='repo-card--link'><a className='repo-card--link' href={repo.svn_url}>Git Home</a></li>
+          </ul>
         </li>
       )
     );
