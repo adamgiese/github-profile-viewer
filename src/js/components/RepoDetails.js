9f7a546cf0875044e6010bdaf83c539939219e22
Adam Giese <adam.giese@gmail.com>
2017-08-06 15:45:33 -0500

    Additional Repo templating and styling

--- src/js/components/RepoDetails.js
+++ src/js/components/RepoDetails.js
@@ -7,6 +7,9 @@ const RepoDetails = props => (
   <div className='repo-details'>
     <Link className='back-link' to='/'>&laquo; Back</Link>
     <h2 className='repo-details--title'>{props.details.name}</h2>
+    <p className='repo-details--language'>{props.details.language}</p>
+    <p className='repo-details--description'>{props.details.description}</p>
+    <a href="https://github.com/adamgiese/react-boilerplate" className='repo-details--link'>Link to Github &raquo;</a>
   </div>
 );
 
